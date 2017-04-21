c7bc4a793397c99b8ce7d801204b793041282f40
SeungChan-PC\SeungChan <blueblack6705@gmail.com>
2017-04-21 20:50:14 +0900

    init 12

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -11,27 +11,47 @@ public class ProductDao {
     }
 
 
-    public Product get(Long id) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new GetProjectStatementStrategy(id);
-
-        return jdbcContext.jdbcContextWithStatementStrategyForQuery(statementStrategy);
+    public Product get(final Long id) throws ClassNotFoundException, SQLException {
+        return jdbcContext.jdbcContextWithStatementStrategyForQuery(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("select * from productinfo where id = ?");
+                preparedStatement.setLong(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
 
 
-    public void add(Product product) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new AddProjectStatementStrategy(product);
+    public void add(final Product product) throws ClassNotFoundException, SQLException {
 
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("insert into productinfo (id, title, price) VALUES (?,?,?)");
+                preparedStatement.setLong(1, product.getId());
+                preparedStatement.setString(2, product.getTitle());
+                preparedStatement.setInt(3, product.getPrice());
+                return preparedStatement;            }
+        });
     }
 
 
 
-    public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteProjectStatementStrategy(id);
+    public void delete(final Long id) throws SQLException {
 
-
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("delete from productinfo where id = ?");
+                preparedStatement.setLong(1, (Long) id);
+                return preparedStatement;
+            }
+        });
     }
 
 
