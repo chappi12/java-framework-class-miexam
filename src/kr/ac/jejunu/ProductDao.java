e4513a9c249750d76196c3ef1fa22c31b419eb77
SeungChan-PC\SeungChan <blueblack6705@gmail.com>
2017-04-21 17:23:44 +0900

    init 1st

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class ProductDao {
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/jeju", "root", "1234");
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/chappi", "root", "1234");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from productinfo where id = ?");
         preparedStatement.setLong(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
@@ -19,4 +19,19 @@ public class ProductDao {
         connection.close();
         return product;
     }
+
+    public void add(Product product) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/chappi", "root", "1234");
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into productinfo (id, title, price) VALUES (?,?,?)");
+        preparedStatement.setLong(1, product.getId());
+        preparedStatement.setString(2, product.getTitle());
+        preparedStatement.setInt(3, product.getPrice());
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    //"delete from userinfo where id = ?"
 }
--- test/kr/ac/jejunu/ProductDaoTest.java
+++ test/kr/ac/jejunu/ProductDaoTest.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +20,26 @@ public class ProductDaoTest {
         assertThat(id, is(product.getId()));
         assertThat(title, is(product.getTitle()));
         assertThat(price, is(product.getPrice()));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+
+        Long id = Long.valueOf(new Random().nextInt(5000));
+        String title = "오렌지";
+        Integer price = 20000;
+
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+
+        ProductDao productDao = new ProductDao();
+        productDao.add(product);
+        Product check = productDao.get(id);
+
+        assertThat(id, is(check.getId()));
+        assertThat(title, is(check.getTitle()));
+        assertThat(price, is(check.getPrice()));
     }
 }
